47e06f8f89a48614c5c064fc5f6adbc1645742d5
Vitória Freitas <vitoriafreitas110903@gmail.com>
2026-01-24 13:02:46 -0300

    CSS base para orcamentos feito

--- planejamento_finaceiro/src/routes/Orcamentos.jsx
+++ planejamento_finaceiro/src/routes/Orcamentos.jsx
@@ -53,82 +53,80 @@ const Orcamentos = () => {
             <input className="orcamento-input" type="number" placeholder="0,00" onChange={(e) =>setOrcamentoNum(e.target.value)}/>
             <button className="orcamento-button" onClick={adicionarOrcamento}>+ Adicionar Orçamento</button>
         </div>
-       
-       
+            
         <div className="orcamentos-ativos-div">
-          <h3>Valor gasto no mes mais recente x Orçamento mensal</h3>
+          <h3>Valor gasto no mês mais recente x Orçamento mensal</h3>
           
-          {valores.moradia && 
+          {orcamentosSalvos.moradia && 
           <div className="orcamento-container">
             <h4>Moradia</h4>
             <div className="orcamento-div1">
               <div className="span-div">
-                <span>R$ {valores.moradia.valor}</span> 
-                <span>de R${orcamentosSalvos.moradia}</span>
+                <span className="span-valor">R$ {valores.moradia ? valores.moradia.valor : 0}</span> 
+                <span className="span-valor-orcamento">de R${orcamentosSalvos.moradia}</span>
               </div>
-              <input className="input-range" type="range" value={valores.moradia.valor} max={orcamentosSalvos.moradia}/>
+              <input className="input-range" type="range" value={valores.moradia ? valores.moradia.valor : 0 } max={orcamentosSalvos.moradia}/>
             </div>
 
             <div className="orcamento-div2">
-              <p>{valores.moradia.porcentagem}% utilizado</p>
-              <p>R${valores.moradia.restante} disponível</p>
+              <p >{valores.moradia ? valores.moradia.porcentagem : 0}% utilizado</p>
+              <p className="p-valor">R$ {valores.moradia ? valores.moradia.restante : orcamentosSalvos.moradia} disponível</p>
             </div>
           </div>
           }
           
-          {valores.transporte && 
+          {orcamentosSalvos.transporte && 
           <div className="orcamento-container">
             <h4>Transporte</h4>
             <div className="orcamento-div1">
               
               <div className="span-div">
-                <span>R${valores.transporte.valor}</span> 
-                <span>de R${orcamentosSalvos.transporte}</span>
+                <span className="span-valor">R$ {valores.transporte ? orcamentosSalvos.valor : 0}</span> 
+                <span className="span-valor-orcamento">de R${orcamentosSalvos.transporte}</span>
               </div>
 
-              <input className="input-range" type="range" value={valores.transporte.valor} max={orcamentosSalvos.transporte}/>
+              <input className="input-range" type="range" value={valores.transporte ? orcamentosSalvos.valor : 0} max={orcamentosSalvos.transporte}/>
               
             </div>
 
             <div className="orcamento-div2">
-              <p>{valores.transporte.porcentagem}% utilizado</p>
-              <p>R${valores.transporte.restante} disponível</p>
+              <p >{valores.transporte ? orcamentosSalvos.porcentagem : 0}% utilizado</p>
+              <p className="p-valor">R$ {valores.transporte ? orcamentosSalvos.restante : orcamentosSalvos.transporte} disponível</p>
             </div>
           </div>
           }
         
-          {valores.alimentacao && 
+          {orcamentosSalvos.alimentacao && 
           <div className="orcamento-container">
             <h4>Alimentacao</h4>
             <div className="orcamento-div1">
               <div className="span-div">
-                <span>R${valores.alimentacao.valor}</span> 
-                <span>de R${orcamentosSalvos.alimentacao}</span>
+                <span className="span-valor">R$ {valores.alimentacao ? valores.alimentacao.valor : 0}</span> 
+                <span className="span-valor-orcamento">de R${orcamentosSalvos.alimentacao}</span>
               </div>
-              <input className="input-range" type="range" value={valores.alimentacao.valor} max={orcamentosSalvos.alimentacao}/>
-              
+              <input className="input-range" type="range" value={valores.alimentacao ? valores.alimentacao.valor : 0} max={orcamentosSalvos.alimentacao}/>   
             </div>
 
             <div className="orcamento-div2">
-              <p>{valores.alimentacao.porcentagem}% utilizado</p>
-              <p>R${valores.alimentacao.restante} disponível</p>
+              <p >{valores.alimentacao ? valores.alimentacao.porcentagem : 0}% utilizado</p>
+              <p className="p-valor">R$ {valores.alimentacao ? valores.alimentacao.restante : orcamentosSalvos.alimentacao} disponível</p>
             </div>
           </div>}
       
-          {valores.Lazer && 
+          {orcamentosSalvos.Lazer && 
           <div className="orcamento-container">
             <h4>Lazer</h4>
             <div className="orcamento-div1">
               <div className="span-div">
-                <span>R${valores.Lazer.valor}</span> 
-                <span>de R${orcamentosSalvos.Lazer}</span>
+                <span className="span-valor">R$ {valores.Lazer ? valores.Lazer.valor : 0}</span> 
+                <span className="span-valor-orcamento">de R${orcamentosSalvos.Lazer}</span>
               </div>
-              <input className="input-range" type="range" value={valores.Lazer.valor} max={orcamentosSalvos.Lazer}/>
+              <input className="input-range" type="range" value={valores.Lazer ? valores.Lazer.valor : 0} max={orcamentosSalvos.Lazer}/>
             </div>
 
             <div className="orcamento-div2">
-              <p>{valores.Lazer.porcentagem}% utilizado</p>
-              <p>R${valores.Lazer.restante} disponível</p>
+              <p>{valores.Lazer ? valores.Lazer.porcentagem : 0}% utilizado</p>
+              <p className="p-valor">R$ {valores.Lazer ? valores.Lazer.restante : orcamentosSalvos.Lazer} disponível</p>
             </div>
           </div>}
     
