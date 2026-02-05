022b47a620fae700bfb7e41d89cdb4454100a837
Vitória Freitas <vitoriafreitas110903@gmail.com>
2026-02-05 09:51:24 -0300

    Bibliotecas para testes instaladas

--- planejamento_finaceiro/vite.config.js
+++ planejamento_finaceiro/vite.config.js
@@ -1,7 +1,13 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom"
+  }
 })
